3cbe014ea1179db99c95c733653365c0bb18fee1
Peter Kim <peter-minho-kim@gmail.com>
2018-12-10 11:37:39 -0500

    Initial commit

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const AppRouter = () => (
+  <BrowserRouter>
+      <Switch>
+        {/*<Route path="/" component={HomePage} exact={true} />*/}
+      </Switch>
+  </BrowserRouter>
+)
+
+export default AppRouter
