d1af6ffe95a8978ec3ecf5a9ae80011342e37384
Peter Kim <peter-minho-kim@gmail.com>
2018-12-12 11:22:52 -0500

    Adds about content

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className="hello-container">
-      hello
+    <div>
+      <div className="hello-container">
+        hello
+      </div>
+
+      <div className="about">
+        <div className="about__portrait"></div>
+        <div className="about__content">
+          <p className="about__content__text">
+            I'm Peter Kim, front-end developer, user experience aficionado, and lover of great design.            
+          </p>
+          <p className="about__content__text">
+            Connect with me on LinkedIn and GitHub.           
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
