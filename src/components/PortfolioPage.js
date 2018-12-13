2f3381e01bbe855434f6fce472d205c712672beb
Peter Kim <peter-minho-kim@gmail.com>
2018-12-12 19:24:10 -0500

    Adds project tabs and project placeholder

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -4,12 +4,26 @@ import Navigation from './Navigation'
 class PortfolioPage extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      selectedProject: 'lucidity'
+    }
   }
   render() {
     return (
       <div>
         <Navigation history={this.props.history} />
-        <h1>THIS IS THE PORTFOLIO PAGE</h1>
+        <div className="project-tabs">
+          <h2 className="project-tabs__title">Lucidity</h2>
+          <h2 className="project-tabs__title">CryptiQ</h2>
+        </div>
+        <div className="project-wrapper">
+          <a href="#">
+            <img src="//unsplash.it/1100/1100" alt="" />
+            <p className="project-text">
+              THIS IS WHERE THE PROJECT INFORMATION TEXT GOES
+            </p>
+          </a>
+        </div>
       </div>
     )
   }
