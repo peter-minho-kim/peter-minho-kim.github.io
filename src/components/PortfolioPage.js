6b2a2b039d4d47b8204985db5fa30971da5fccb5
Peter Kim <peter-minho-kim@gmail.com>
2018-12-13 14:24:05 -0500

    Adds conditional rendering for video text

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -5,20 +5,34 @@ class PortfolioPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selectedProject: 'lucidity'
+      projectView: 'lucidity',
+      projectUrl: 'https://lucidity-app.herokuapp.com/',
+      projectPath: './videos/lucidity-demo.mp4'
     }
     this.handleLuciditySelect = this.handleLuciditySelect.bind(this)
     this.handleCryptiqSelect = this.handleCryptiqSelect.bind(this)
   }
   handleLuciditySelect() {
-    this.setState(() => ({ selectedProject: 'lucidity' }))
+    this.setState(() => {
+      return {
+        projectView: 'lucidity',
+        projectUrl: 'https://lucidity-app.herokuapp.com/',
+        projectPath: './videos/lucidity-demo.mp4'
+      }
+    })
     this.refs.lucidityLink.classList.add('active-link')
     this.refs.lucidityLink.classList.remove('inactive-link')
     this.refs.cryptiqLink.classList.remove('active-link')
     this.refs.cryptiqLink.classList.add('inactive-link')
   }
   handleCryptiqSelect() {
-    this.setState(() => ({ selectedProject: 'cryptiq' }))
+    this.setState(() => {
+      return {
+        projectView: 'cryptiq',
+        projectUrl: 'https://cryptiq.herokuapp.com/',
+        projectPath: './videos/cryptiq-demo.mp4'
+      }
+    })
     this.refs.cryptiqLink.classList.remove('inactive-link')
     this.refs.cryptiqLink.classList.add('active-link')
     this.refs.lucidityLink.classList.add('inactive-link')
@@ -46,25 +60,22 @@ class PortfolioPage extends React.Component {
         </div>
 
         <div className="project-wrapper">
-
           <div className="project-video-box project-video-box--lucidity">
-            <a href="https://lucidity-app.herokuapp.com/" target="_blank">
-              <video src="./videos/lucidity-demo.mp4" autoPlay loop muted preload="auto" className="video-demo"></video>
-              <p className="project-text">
-                LUCIDITY LUCIDITY LUCIDITY LUCIDITY LUCIDITY LUCIDITY 
-              </p>
+            <a href={this.state.projectUrl} target="_blank">
+              <video src={this.state.projectPath} autoPlay loop muted preload="auto" className="video-demo"></video>
+              <div>
+                {this.state.projectView === 'lucidity' ? 
+                  <p className="project-text">
+                    LUCIDITY LUCIDITY 
+                  </p>
+                  :
+                  <p className="project-text">
+                    CRYPTIQ CRYPTIQ
+                  </p>
+                }
+              </div>
             </a>
-          </div>
-
-          <div className="project-video-box project-video-box--cryptiq">
-            <a href="https://cryptiq.herokuapp.com/" target="_blank">
-              <video src="./videos/cryptiq-demo.mp4" autoPlay loop muted preload="auto" className="video-demo"></video>
-              <p className="project-text">
-                CRYPTIQ CRYPTIQ CRYPTIQ CRYPTIQ CRYPTIQ CRYPTIQ 
-              </p>
-            </a>
-          </div>
-
+          </div>         
         </div>
       </div>
     )
