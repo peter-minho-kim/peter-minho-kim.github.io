023be04fd607cd7910478ae7090fae6d10ce7870
Peter Kim <peter-minho-kim@gmail.com>
2018-12-13 00:18:13 -0500

    Adds videos

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -7,22 +7,64 @@ class PortfolioPage extends React.Component {
     this.state = {
       selectedProject: 'lucidity'
     }
+    this.handleLuciditySelect = this.handleLuciditySelect.bind(this)
+    this.handleCryptiqSelect = this.handleCryptiqSelect.bind(this)
+  }
+  handleLuciditySelect() {
+    this.setState(() => ({ selectedProject: 'lucidity' }))
+    this.refs.lucidityLink.classList.add('active-link')
+    this.refs.lucidityLink.classList.remove('inactive-link')
+    this.refs.cryptiqLink.classList.remove('active-link')
+    this.refs.cryptiqLink.classList.add('inactive-link')
+  }
+  handleCryptiqSelect() {
+    this.setState(() => ({ selectedProject: 'cryptiq' }))
+    this.refs.cryptiqLink.classList.remove('inactive-link')
+    this.refs.cryptiqLink.classList.add('active-link')
+    this.refs.lucidityLink.classList.add('inactive-link')
+    this.refs.lucidityLink.classList.remove('active-link')
   }
   render() {
     return (
       <div>
         <Navigation history={this.props.history} />
         <div className="project-tabs">
-          <h2 className="project-tabs__title">Lucidity</h2>
-          <h2 className="project-tabs__title">CryptiQ</h2>
+          <h2 
+            className="project-tabs__title active-link" 
+            onClick={this.handleLuciditySelect}
+            ref="lucidityLink"
+          >
+            Lucidity
+          </h2>
+          <h2 
+            className="project-tabs__title inactive-link" 
+            onClick={this.handleCryptiqSelect}
+            ref="cryptiqLink"
+          >
+            CryptiQ
+          </h2>
         </div>
+
         <div className="project-wrapper">
-          <a href="#">
-            <img src="//unsplash.it/1100/1100" alt="" />
-            <p className="project-text">
-              THIS IS WHERE THE PROJECT INFORMATION TEXT GOES
-            </p>
-          </a>
+
+          <div className="project-video-box project-video-box--lucidity">
+            <a href="https://lucidity-app.herokuapp.com/" target="_blank">
+              <video src="./videos/lucidity-demo.mp4" autoPlay loop muted preload="auto" className="video-demo"></video>
+              <p className="project-text">
+                LUCIDITY LUCIDITY LUCIDITY LUCIDITY LUCIDITY LUCIDITY 
+              </p>
+            </a>
+          </div>
+
+          <div className="project-video-box project-video-box--cryptiq">
+            <a href="https://cryptiq.herokuapp.com/" target="_blank">
+              <video src="./videos/cryptiq-demo.mp4" autoPlay loop muted preload="auto" className="video-demo"></video>
+              <p className="project-text">
+                CRYPTIQ CRYPTIQ CRYPTIQ CRYPTIQ CRYPTIQ CRYPTIQ 
+              </p>
+            </a>
+          </div>
+
         </div>
       </div>
     )
